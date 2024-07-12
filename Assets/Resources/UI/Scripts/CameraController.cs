7448aa979980ab9141339ea172c89035e86070aa
KoKeRsfu <koker.garlicbread@gmail.com>
2024-07-12 17:04:41 +0700

    3

---
+++ Assets/Resources/UI/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField] Camera cam;
+	private GameObject camTarget;
+	
+	public float camMovementMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {    
+	    camTarget = this.transform.GetChild(0).gameObject;	
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		Vector2 mousePos = cam.ScreenToViewportPoint(Input.mousePosition);
+		camTarget.transform.position = new Vector2(this.transform.position.x + (mousePos.x * camMovementMultiplier), this.transform.position.y + (mousePos.y * camMovementMultiplier));
+		//Debug.Log(Vector2.Distance(this.transform.position, new Vector2()));
+	}
+    
+	public void MoveToOptions() 
+	{
+		transform.position = new Vector2(-18,0);
+	}
+	
+	public void MoveToMenu() 
+	{
+		transform.position = new Vector2(0, 0);
+	}
+}
---
+++ Assets/Resources/UI/Scripts/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+	[SerializeField] GameObject soundPlayer;
+	[SerializeField] AudioClip clickSound;
+	
+	[SerializeField] CameraController camController;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+	/*
+	private void ClickSound() 
+	{
+		soundPlayer a = Instantiate(soundPlayer).GetComponent<soundPlayer>();
+		a.track = clickSound;
+	}
+	*/
+	
+	public void PlayButton()
+	{
+		SceneManager.LoadScene(1);
+		//ClickSound();
+	}
+	
+	public void OptionsButton() 
+	{
+		camController.MoveToOptions();
+		//ClickSound();
+	}
+	
+	public void MenuButton() 
+	{
+		camController.MoveToMenu();
+		//ClickSound();
+	}
+	
+	public void ExitButton() 
+	{
+		//ClickSound();
+		Application.Quit();
+	}
+}
