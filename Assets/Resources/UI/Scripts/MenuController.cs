bf92269adf0b88dd5e8efd73cfb211ff2170b0f3
KoKeRsfu <koker.garlicbread@gmail.com>
2024-07-14 16:00:37 +0700

    musicplayer2!

--- Assets/Resources/UI/Scripts/MenuController.cs
+++ Assets/Resources/UI/Scripts/MenuController.cs
@@ -14,11 +14,19 @@ public class MenuController : MonoBehaviour
 	[SerializeField] Slider MusicSlider;
 	[SerializeField] Slider SoundSlider;
 	
+	public AudioClip menuTheme;
+	public MusicPlayer musicPlayer;
+	
     // Start is called before the first frame update
     void Start()
-    {
+	{
+				
+		musicPlayer = GameObject.FindGameObjectWithTag("musicplayer").GetComponent<MusicPlayer>();
+		
 	    ChangeMusicVolume(PlayerPrefs.GetFloat("MusicVolume", 0.5f));
-	    ChangeSoundVolume(PlayerPrefs.GetFloat("SoundVolume", 0.5f));
+		ChangeSoundVolume(PlayerPrefs.GetFloat("SoundVolume", 0.5f));
+		
+		musicPlayer.ChangeTrack(menuTheme);
     }
 
     // Update is called once per frame
@@ -40,6 +48,7 @@ public class MenuController : MonoBehaviour
 	{
 		PlayerPrefs.SetFloat("MusicVolume", a);
 		MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+		musicPlayer.audiosource.volume = a;
 	}
 	
 	public void ChangeSoundVolume(float a) 
